86fb2963eed6678e81dcc2e629ed312742123fbb
abhishank05 <abhishekshank05@gmail.com>
2025-02-10 23:20:09 +0530

    commited changes on 10/2/25

--- controllers/routes.py
+++ controllers/routes.py
@@ -8,7 +8,7 @@ from flask import current_app as app
 def home():
     return render_template("index.html")
 
-'''
+
 @app.route("/login",methods=["GET","POST"])
 def signin():
     if request.method=="POST":
@@ -16,18 +16,13 @@ def signin():
         pwd=request.form.get("password")
         usr=User_Info.query.filter_by(email=uname,password=pwd).first()
         if usr and usr.qualification=="Administrator":
-            print("User found:", usr.email)
-        if usr.password == pwd:
-            print("Password matches!")
-            session['user_id'] = usr.id
             return render_template("admin_dashboard.html")
+        elif usr and usr.qualification!="Administrator":
+            return render_template("user_dashboard.html")
         else:
-            print("Password mismatch!")
-    else:
-        print("User not found")
-           # return render_template("admin_dashboard.html")
+            return render_template("login.html",msg="Invalid user Credentials ...")
 
-    return render_template("login.html")
+    return render_template("login.html",msg="")
 '''
 @app.route("/login", methods=["GET", "POST"])
 def signin():
@@ -62,7 +57,7 @@ def signin():
             return render_template("login.html", error="User does not exist.")  # Handle NoneType here
 
     return render_template("login.html")
-
+'''
 
 @app.route("/register",methods=["GET","POST"])
 def signup():
@@ -74,6 +69,8 @@ def signup():
         dob=request.form.get("dob")
         dob_date = datetime.strptime(dob, '%Y-%m-%d').date()    
         new_usr=User_Info(email=uname,password=pwd,full_name=fname,qualification=qual,dob=dob_date)
+        if new_usr:
+            return render_template("signup.html",msg="Sorry,the mail is already registered!!!")
         db.session.add(new_usr)
         db.session.commit()
         return render_template("login.html")
